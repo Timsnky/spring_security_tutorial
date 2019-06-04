b59e20ce7d7c6f074d4ca8411f10762af16cc697
Timothy Kimathi <timsnky@gmail.com>
2019-06-04 17:11:44 +0300

    Implemented user details and user details service classes for integration with spring security

---
+++ src/main/java/app/timsnky/spring_security_tutorial/UserPrincipalDetailsService.java
@@ -0,0 +1,25 @@
+package app.timsnky.spring_security_tutorial;
+
+import app.timsnky.spring_security_tutorial.db.UserRepository;
+import app.timsnky.spring_security_tutorial.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+
+        return new UserPrincipal(user);
+    }
+}
